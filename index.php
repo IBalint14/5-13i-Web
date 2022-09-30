cfe35e518fc4f5b38efb21e79b9855f49d0e96e7
IBalint14 <112863413+IBalint14@users.noreply.github.com>
2022-09-30 13:48:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,13 +25,12 @@
 
 		$osztaly = 1;
 
-		if(isset($_GET['osztalyNeve'])) {
-			$osztaly = $_GET['osztalyNeve'];
+		if(isset($_GET['osztalyId'])) {
+			$osztaly = $_GET['osztalyId'];
 		}
 		$osztalyok = array();
 
-	
-		$sajatMagam = array('osztaly'=> "13 i",'sorom' => 2, 'oszlopom' => 2);
+		
 		$sql = "SELECT osztalyId, osztalyNev FROM osztalyok";
 		$result = $conn->query($sql);
 
@@ -47,17 +46,44 @@
 
 			//$kulcsok = array_keys($osztalyok);
 
+			if(!array_key_exists($osztaly, $osztalyok)){
+				$osztaly = 1;
+			}
 	?>
 		 <title><?php echo "$osztalyok[$osztaly]" ?></title>
 </head>
 <body>
 	<?php
+		print "A post tartamla: ";
+		print_r($_POST);
+		if(isset($_POST['keresettNev'])){
+			echo $sql = "SELECT osztalyId FROM szemelyek INNER JOIN sorok 
+			ON (szemelyek.szemelyId = sorok.nev1 
+				OR szemelyek.szemelyId = sorok.nev2
+				OR szemelyek.szemelyId = sorok.nev3 
+				OR szemelyek.szemelyId = sorok.nev4 
+				OR szemelyek.szemelyId = sorok.nev5) 
+			WHERE nev LIKE '".$_POST['keresettNev']."%';";
+			$result = $conn->query($sql);
+			if($result ->num_rows > 0){
+				$row = $result->fetch_assoc();
+				print_r($row);
+				$osztaly = $row['osztalyId'];
+			}
+		}
+			// SELECT osztalyId FROM `szemelyek` INNER JOIN sorok ON (szemelyek.szemelyId = sorok.nev1 OR szemelyek.szemelyId = sorok.nev2 OR szemelyek.szemelyId = sorok.nev3 OR szemelyek.szemelyId = sorok.nev4 OR szemelyek.szemelyId = sorok.nev5) WHERE nev LIKE 'Tóth Kristóf';
 
 		echo "<h1>$osztalyok[$osztaly]</h1>";
-					
+		?>
+		<form method="post" action="index.php">
+			<input type="text" name="keresettNev">
+			<input type="submit" value="KERES">
+		</form>	
+	
+	<?php	
 		foreach($osztalyok as $kulcs => $ertek){
-				if($kulcs != $osztaly){		
-					echo "<a href = \"index.php?osztalyNeve=$kulcs\"> $ertek </a><br>";			
+				if($kulcs != $osztaly){
+					echo "<a href = \"index.php?osztalyId=$kulcs\"> $ertek </a><br>";			
 				}
 			
 		}
@@ -66,33 +92,43 @@
     <table >
 
 		<?php
+			$sajatMagam = array('sorId'=> 7, 'mezoNeve' => 'nev3');
 
-			$sql = "SELECT nev1, nev2, nev3, nev4, nev5 FROM sorok WHERE osztalyId = ".$osztaly;
+			$sql = "SELECT sorId, nev1, nev2, nev3, nev4, nev5 FROM sorok WHERE osztalyId = ".$osztaly;
 			$result = $conn->query($sql);
 
 				if($result->num_rows > 0){
 					echo '<table>';
 					while($row = $result->fetch_assoc()){
-						//print_r($row). "<br>";
-						//$osztalyok[$row['osztalyId']] = $row['osztalyNev'];
-						echo "<tr>";
-						echo "<td>".$row['nev1']."</td>\n";
-						echo "<td>".$row['nev2']."</td>\n";
-						echo "<td>".$row['nev3']."</td>\n";
-						echo "<td>".$row['nev4']."</td>\n";
-						echo "<td>".$row['nev5']."</td>\n";
-						echo "</tr>";
-					}
-					echo'</table>';
-				}					
-				else{
-					echo "Nincsenek tanulók eben az osztályban";
+				//print_r($row). "<br>";
+				//$osztalyok[$row['osztalyId']] = $row['osztalyNev'];
+					echo "<tr>";
+					for($i=1; $i<6; $i++){
+						$nev = "_";
+						$mezoNev = 'nev'.$i;
+						if($row[$mezoNev] != null ){
+							$sql = "SELECT nev FROM	szemelyek WHERE szemelyId = ".$row[$mezoNev];
+							$result2 = $conn->query($sql);
+							if($result->num_rows > 0){
+								$szemelySor = $result2->fetch_assoc();
+								$nev = $szemelySor['nev']; 
+							}
+						}						
+						if($row['sorId'] == $sajatMagam ['sorId'] and $sajatMagam['mezoNeve'] == $mezoNev){
+							echo "<td style=\"background-color:rgb(105, 219, 105);\">".$nev."</td>\n";
+						} else {
+							echo "<td>".$nev."</td>\n";
+						}
+							
 				}
-					//Erre most semmi szükségünk sincs
-					if($osztaly == $sajatMagam['osztaly'] and $sor == $sajatMagam['sorom'] and $oszlop == $sajatMagam['oszlopom']) {
-						echo "<td class = 'magam'>$tanulo</td>";
-		}					
-			//else echo "<h3>Nincs ilyen osztály: $osztaly</h3>";
+					echo "</tr>";
+				}
+				echo'</table>';
+				}					
+			else{
+				echo "Nincsenek tanulók eben az osztályban";
+			}			
+		//else echo "<h3>Nincs ilyen osztály: $osztaly</h3>";
 			$conn->close();
 		?>
 </body>
